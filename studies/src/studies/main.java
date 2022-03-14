935dfab6e01fe4078670464ffe66a106fb3c938f
kelly <kelly@KELLY>
2022-03-13 23:49:00 -0300

    Array unidimensional (Vetor) – Entrada automática

---
+++ studies/src/studies/main.java
@@ -0,0 +1,21 @@
+package studies;
+
+public class main
+{
+	public static void main(String arg[])
+	{
+			int larg=30;
+			int vtr[]= new int[larg];
+			System.out.println("O tamanho do vetor eh: "+vtr.length);
+			int cont=0;
+			for(int i=0;i<vtr.length;i++)
+			{
+				cont=cont+2;
+				vtr[i]=cont;
+			}
+			for(int i=0;i<vtr.length;i++)
+			{
+				System.out.println("Vetor no local "+i+" tem valor "+vtr[i]);
+			}
+	}
+}
